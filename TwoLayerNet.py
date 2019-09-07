gradient check have something wrong!

TwoLayerNet.py
@@ -7,14 +7,14 @@ from collections import OrderedDict
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         # 初始化权重
-        self.params = {}
-        # self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params["W1"] = np.random.randn(input_size, hidden_size) / np.sqrt(input_size)
-        self.params["b1"] = np.zeros(hidden_size)
-        # self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params["W2"] = np.random.randn(hidden_size, output_size) / np.sqrt(hidden_size)
-        self.params["b2"] = np.zeros(output_size)
-
+        # self.params = {"W1": np.random.randn(input_size, hidden_size) / np.sqrt(input_size),
+        #                "b1": np.zeros(hidden_size),
+        #                "W2": np.random.randn(hidden_size, output_size) / np.sqrt(hidden_size),
+        #                "b2": np.zeros(output_size)}
+        self.params = {"W1": weight_init_std * np.random.randn(input_size, hidden_size),
+                       "b1": np.zeros(hidden_size),
+                       "W2": weight_init_std * np.random.randn(hidden_size, output_size),
+                       "b2": np.zeros(output_size)}
         # 生成层
         self.layers = OrderedDict()
         self.layers["Affine1"] = Affine(self.params["W1"], self.params["b1"])
@@ -44,12 +44,10 @@ class TwoLayerNet:
 
     def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
-        grads = {}
-
-        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        grads = {'W1': numerical_gradient(loss_W, self.params['W1']),
+                 'b1': numerical_gradient(loss_W, self.params['b1']),
+                 'W2': numerical_gradient(loss_W, self.params['W2']),
+                 'b2': numerical_gradient(loss_W, self.params['b2'])}
 
         return grads
 
@@ -66,11 +64,8 @@ class TwoLayerNet:
         for layer in layers:
             dout = layer.backward(dout)
 
-        grads = {}
-        grads['W1'] = self.layers["Affine1"].dW
-        grads['b1'] = self.layers["Affine1"].db
-        grads['W2'] = self.layers["Affine2"].dW
-        grads['b2'] = self.layers["Affine2"].db
+        grads = {'W1': self.layers["Affine1"].dW, 'b1': self.layers["Affine1"].db, 'W2': self.layers["Affine2"].dW,
+                 'b2': self.layers["Affine2"].db}
 
         return grads
 
@@ -78,3 +73,5 @@ class TwoLayerNet:
 if __name__ == '__main__':
     net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
     print(net.params["W1"].shape)
+    x = np.random.rand(100, 784)
+    y = net.predict(x)

common/gradient.py
@@ -8,7 +8,7 @@ def numerical_diff(f, x):
 
 
 def numerical_gradient(f, x):
-    h = 1e-4
+    h = 1e-5
     grad = np.zeros_like(x)
 
     it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])